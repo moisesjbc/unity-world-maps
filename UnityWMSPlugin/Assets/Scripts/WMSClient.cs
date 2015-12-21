3e86c483f20c2190da5057e9f987a11837e01374
moisesjbc <moisesjbc@gmail.com>
2015-12-21 12:11:35 +0000

    Added list of visited servers to WMS client.

--- UnityWMSPlugin/Assets/Scripts/WMSClient.cs
+++ UnityWMSPlugin/Assets/Scripts/WMSClient.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 public class WMSClient {
 	private Dictionary<string,WWW> requests_ = new Dictionary<string,WWW>();
+	private List<string> serverURLs = new List<string> ();
 
 	public string Request( string server, string version = "1.1.0" )
 	{
@@ -23,6 +24,8 @@ public class WMSClient {
 	public WMSInfo GetResponse( string requestID )
 	{
 		if (requests_.ContainsKey (requestID) && requests_ [requestID].isDone) {
+			serverURLs.Add( GetHostFromURL(requestID) );
+			Debug.Log ("Server added to list: " + GetHostFromURL(requests_ [requestID].url) );
 			return WMSXMLParser.GetWMSInfo (requests_ [requestID].text);
 		}
 
@@ -33,5 +36,15 @@ public class WMSClient {
 	private string BuildRequestID( string server, string version = "1.1.0" )
 	{
 		return server + "@@@" + version;
+	}
+
+
+	private string GetHostFromURL( string id )
+	{
+		int separatorIndex = id.IndexOf("?");
+		if (separatorIndex > 0){
+			return id.Substring(0, separatorIndex);
+		}
+		return id;
 	}
 }
