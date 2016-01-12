a8dee5a68e18647de757cc0bc3e12932f7637a0b
moisesjbc <moisesjbc@gmail.com>
2016-01-12 08:40:15 +0000

    UnityQuadtreeLODNode integrated into UnityQuadtreeLODPlane.

--- UnityWMSPlugin/Assets/Scripts/Terrain/QuadtreeLODPlane.cs
+++ UnityWMSPlugin/Assets/Scripts/Terrain/QuadtreeLODPlane.cs
@@ -1,76 +1,316 @@
-﻿using UnityEngine;
+﻿//#define PAINT_QUADS
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
+
 public class QuadtreeLODPlane : MonoBehaviour {
-	private QuadtreeLODNode rootNode = null;
 	public int vertexResolution = 20;
-	public OnlineTexturesRequester texturesRequester; 
+	private bool visible_ = true;
+
+	private OnlineTexturesRequester onlineTexturesRequester;
+
+	private string nodeID = "0";
+
+	GameObject[] children_ = new GameObject[]{ null, null, null, null };
+
+	private int depth_ = 0;
+	const int MAX_DEPTH = 7;
+
+	string textureRequestId;
+	bool textureLoaded = false;
+
+	float metersPerUnit = 0.0f;
 
 
 	public void Start()
 	{
-		Reset ();
+		if (depth_ == 0) {
+			Vector3 meshSize = GetComponent<MeshRenderer> ().bounds.size;
+			if (meshSize.x != meshSize.z) {
+				Debug.LogWarning ("LOD plane must be square (currently: " +
+				meshSize.x +
+				"x" +
+				meshSize.y +
+				")");
+			}
+
+			float mapSize = Mathf.Max (meshSize.x, meshSize.z);
+
+			if (GetComponent<WMSComponent> () != null) {
+				onlineTexturesRequester = this.GetComponent<WMSComponent> ();
+
+				if (GetComponent<BingMapsComponent> () != null) {
+					throw new UnityException ("Terrain can't have both WMSComponent and BingMapsComponent componets!");
+				}
+			} else if (GetComponent<BingMapsComponent> () != null) {
+				onlineTexturesRequester = this.GetComponent<BingMapsComponent> ();
+			} else {
+				throw new MissingComponentException ("Terrain must have a WMSComponent or BingMapsComponent");
+			}
+
+			nodeID = "0";
+
+			// Create the root mesh.
+			gameObject.GetComponent<MeshFilter> ().mesh = MeshFactory.CreateMesh (mapSize, vertexResolution);
+
+			// Create material
+			gameObject.GetComponent<Renderer> ().material = new Material (Shader.Find ("Standard"));
+
+			GetComponent<QuadtreeLODPlane> ().SetVisible (true);
+		} else {
+			GetComponent<QuadtreeLODPlane> ().SetVisible (false);
+			onlineTexturesRequester = transform.parent.GetComponent<QuadtreeLODPlane> ().onlineTexturesRequester;
+		}
+		Debug.Log ("[" + nodeID + "] Start() - depth (" + depth_ + ")");
+				
+		gameObject.tag = "MapSector";
+
+		// FIXME: Compute this instead of giving a arbitrary value.
+		metersPerUnit = 1000;
+		Debug.LogWarning ("[" + nodeID + "]: metersPerUnit: " + metersPerUnit);
+
+		textureRequestId = onlineTexturesRequester.RequestTexture (nodeID);
+		textureLoaded = false;
 	}
 
 
-	public void Reset()
+	public GameObject CreateChild( string nodeID, Color color, Vector3 localPosition )
 	{
-		Vector3 meshSize = GetComponent<MeshRenderer> ().bounds.size;
-		if (meshSize.x != meshSize.z) {
-			Debug.LogWarning ("LOD plane must be square (currently: " + 
-			                  meshSize.x + 
-			                  "x" +
-			                  meshSize.y +
-			                  ")");
+		// Create the child game object.
+		// Initially this was done by duplicating current game object, but this copied
+		// children as well and errors arisen.
+		GameObject childGameObject = new GameObject();
+		childGameObject.AddComponent<MeshRenderer>();
+		childGameObject.AddComponent<MeshFilter>();
+		childGameObject.AddComponent<QuadtreeLODPlane>();
+
+		// Copy parent's mesh
+		Mesh parentMesh = gameObject.GetComponent<MeshFilter>().mesh;
+		Mesh childMesh = new Mesh ();
+		childMesh.vertices = parentMesh.vertices;
+		childMesh.triangles = parentMesh.triangles;
+		childMesh.uv = parentMesh.uv;
+		childMesh.RecalculateNormals ();
+		childMesh.RecalculateBounds ();
+		childGameObject.GetComponent<MeshFilter> ().mesh = childMesh;
+
+		// Make this child transform relative to parent.
+		childGameObject.transform.parent = gameObject.transform;
+
+		// Previous assignment alters local transformation, so we reset it.
+		childGameObject.transform.localRotation = Quaternion.identity;
+		childGameObject.transform.localPosition = localPosition;
+
+		childGameObject.transform.localScale = new Vector3( 0.5f, 1.0f, 0.5f );
+
+		#if PAINT_QUADS
+			childGameObject.GetComponent<Renderer>().material.color = color;
+		#endif
+		childGameObject.GetComponent<QuadtreeLODPlane>().depth_ = this.depth_ + 1;
+		childGameObject.GetComponent<QuadtreeLODPlane> ().nodeID = nodeID;
+		childGameObject.GetComponent<QuadtreeLODPlane>().children_ = new GameObject[]{ null, null, null, null };
+
+
+		return childGameObject;
+	}
+
+
+	private void FlipUV()
+	{
+		Vector2[] uv = gameObject.GetComponent<MeshFilter> ().mesh.uv;
+		for (int i=0; i<uv.Length; i++) {
+			uv[i].x = 1.0f - uv[i].x;
+			uv[i].y = 1.0f - uv[i].y;
+		}
+		gameObject.GetComponent<MeshFilter> ().mesh.uv = uv;
+	}
+
+
+	public void SetVisible( bool visible )
+	{
+		// Set node visibility.
+		visible_ = visible;
+		gameObject.GetComponent<MeshRenderer> ().enabled = visible;
+
+		// Enable or disable collider according to new visibility value.
+		Collider collider = gameObject.GetComponent<Collider>();
+		if ( collider != null ) {
+			collider.enabled = visible;
 		}
 
-		float mapSize = Mathf.Max ( meshSize.x, meshSize.z );
-		
-		if (rootNode != null) {
-			GameObject[] mapSectors = GameObject.FindGameObjectsWithTag ("MapSector");
-			for( int i=0; i<mapSectors.Length; i++ ){
-				Destroy (mapSectors[i]);
+		// No matter which visibility is applied to this node, children
+		// visibility must be set to false.
+		if (children_ != null && children_[0] != null) {
+			for( int i = 0; i < children_.Length; i++ ){
+				children_[i].GetComponent<QuadtreeLODPlane>().SetVisible (false);
 			}
 		}
-			
-		if (GetComponent<WMSComponent> () != null) {
-			texturesRequester = this.GetComponent<WMSComponent> ();
-
-			if (GetComponent<BingMapsComponent> () != null) {
-				throw new UnityException ("Terrain can't have both WMSComponent and BingMapsComponent componets!");
-			}
-		} else if (GetComponent<BingMapsComponent> () != null) {
-			texturesRequester = this.GetComponent<BingMapsComponent> ();
-		} else {
-			throw new MissingComponentException ("Terrain must have a WMSComponent or BingMapsComponent");
-		}
-			
-		rootNode = new QuadtreeLODNode( 
-		                               mapSize, 
-									   vertexResolution,
-		                               transform, 
-		                               this.GetComponent<Material>(),
-									   texturesRequester
-		                               );
-		GetComponent<MeshRenderer> ().enabled = false;
 	}
 
 
 	void Update () {
-		if (rootNode != null) {
-			rootNode.Update ( false );
+		if (visible_ || AreChildrenLoaded()) {
+			DistanceTestResult distanceTestResult = DoDistanceTest();
+			Vector3 meshSize = Vector3.Scale (GetComponent<MeshFilter>().mesh.bounds.size, gameObject.transform.lossyScale);
+
+			// Subdivide the plane if camera is closer than a threshold.
+			if (visible_ && distanceTestResult == DistanceTestResult.SUBDIVIDE ) {
+				// Create children if they don't exist.
+				if (depth_ < MAX_DEPTH && children_ [0] == null) {
+					CreateChildren (meshSize);
+				}
+
+				// Make this node invisible and children visible.
+				if (AreChildrenLoaded ()) {
+					SetVisible (false);
+					for (int i = 0; i < children_.Length; i++) {
+						children_ [i].GetComponent<QuadtreeLODPlane>().SetVisible (true);
+					}
+				}
+			}else if ( !visible_ && AreChildrenLoaded () && distanceTestResult == DistanceTestResult.JOIN ) {
+				SetVisible (true);
+				for (int i = 0; i < children_.Length; i++) {
+					children_ [i].GetComponent<QuadtreeLODPlane>().SetVisible (false);
+				}
+			}
+		}
+			
+		if (!textureLoaded) {
+			Texture2D texture = onlineTexturesRequester.GetTexture (textureRequestId);
+			if (texture != null) {
+				textureLoaded = true;
+				GetComponent<Renderer>().material.mainTexture = texture;
+				GetComponent<Renderer>().material.mainTexture.wrapMode = TextureWrapMode.Clamp;
+			}
 		}
 	}
 
 
-	public float GetHeight( Vector3 observer )
+	enum DistanceTestResult 
 	{
-		float height;
-		if (rootNode != null && rootNode.ObserverOnSector (observer, out height)) {
-			return height;
-		} else {
-			return observer.y;
+		DO_NOTHING,
+		SUBDIVIDE,
+		JOIN
+	}
+
+
+	private DistanceTestResult DoDistanceTest()
+	{
+		const float THRESHOLD_FACTOR = 2.5f;
+
+		Vector3 cameraPos = Camera.main.transform.position;
+		float distanceCameraBorder = Vector3.Distance (cameraPos, gameObject.GetComponent<MeshRenderer> ().bounds.ClosestPoint (cameraPos));
+		Vector3 boundsSize = gameObject.GetComponent<MeshRenderer> ().bounds.size;
+		float radius = (boundsSize.x + boundsSize.y + boundsSize.z) / 3.0f;
+
+		if (distanceCameraBorder < THRESHOLD_FACTOR * radius) {
+			return DistanceTestResult.SUBDIVIDE;
+		} else if (distanceCameraBorder >= THRESHOLD_FACTOR * radius) {
+			return DistanceTestResult.JOIN;
 		}
+
+		return DistanceTestResult.DO_NOTHING;
+	}
+
+
+	private void CreateChildren( Vector3 meshSize )
+	{
+		Debug.Log ("[" + nodeID + "] Creating children");
+
+		Vector3 S = new Vector3(
+			1.0f / gameObject.transform.lossyScale.x,
+			1.0f / gameObject.transform.lossyScale.y,
+			1.0f / gameObject.transform.lossyScale.z
+		);
+
+
+		Vector3[] childLocalPosition = new Vector3[]
+		{
+			Vector3.Scale ( new Vector3( -meshSize.x/4,0,meshSize.z/4 ), S ),
+			Vector3.Scale ( new Vector3( -meshSize.x/4,0,-meshSize.z/4 ), S ),
+			Vector3.Scale ( new Vector3( meshSize.x/4,0,meshSize.z/4), S ),
+			Vector3.Scale ( new Vector3( meshSize.x/4,0,-meshSize.z/4), S )
+		};
+
+
+		Color[] childrenColors = new Color[]
+		{
+			new Color( 1.0f, 0.0f, 0.0f, 1.0f ),
+			new Color( 0.0f, 1.0f, 0.0f, 1.0f ),
+			new Color( 0.0f, 0.0f, 1.0f, 1.0f ),
+			new Color( 1.0f, 1.0f, 0.0f, 1.0f )
+		};
+
+
+		string[] childrenIDs = new string[] 
+		{
+			nodeID + "0",
+			nodeID + "1",
+			nodeID + "2",
+			nodeID + "3"
+		};
+
+		for( int i=0; i<4; i++ ){
+			children_[i] = CreateChild( childrenIDs[i], childrenColors[i], childLocalPosition[i] );
+		}
+	}
+
+
+	private bool AreChildrenLoaded(){
+		if (children_ [0] != null) {
+			for (int i = 0; i < 4; i++) {
+				if (children_ [i].GetComponent<QuadtreeLODPlane>().textureLoaded == false) {
+					return false;
+				}
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+
+	public bool ObserverOnSector( Vector3 observer, out float observerHeight )
+	{
+		if (visible_) {
+			// Compute if the XZ rect of the map sector contains the observer.
+			Vector3 sectorSize = Vector3.Scale (GetComponent<MeshFilter>().mesh.bounds.size, gameObject.transform.lossyScale);
+
+			Rect sectorRect = new Rect (
+				gameObject.transform.position.x - sectorSize.x / 2.0f,
+				gameObject.transform.position.z - sectorSize.z / 2.0f,
+				sectorSize.x,
+				sectorSize.z
+			);
+			//Debug.Log ( "sectorRect: " + sectorRect );
+
+			if (sectorRect.Contains (new Vector2 (observer.x, observer.z))) {
+				MeshCollider mapCollider = gameObject.GetComponent<MeshCollider> ();
+				if (mapCollider != null) {
+					Ray ray = new Ray (observer, Vector3.down);
+					RaycastHit hit;
+
+					if (mapCollider.Raycast (ray, out hit, 100.0f)) {
+						observerHeight = observer.y - hit.point.y;
+						return true;
+					}
+				}
+			}
+		} else {
+			if (AreChildrenLoaded ()) {
+				for (int i=0; i<children_.Length; i++) {
+					float heightOnChildren;
+					if (children_ [i].GetComponent<QuadtreeLODPlane>().ObserverOnSector (observer, out heightOnChildren)) {
+						observerHeight = heightOnChildren;
+						return true;
+					}
+				}
+			}
+		}
+
+		observerHeight = -1.0f;
+		return false;
 	}
 }
