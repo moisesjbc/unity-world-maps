7a61c3bed4c0866927f602074cb8cfd6c5192a52
moisesjbc <moisesjbc@gmail.com>
2016-01-13 10:52:48 +0000

    Minor fix when computing BingMaps initial sector.

--- UnityWMSPlugin/Assets/Scripts/BingMaps/BingMapsComponent.cs
+++ UnityWMSPlugin/Assets/Scripts/BingMaps/BingMapsComponent.cs
@@ -49,7 +49,7 @@ public class BingMapsComponent : OnlineTexturesRequester {
 					maxLattitude -= halfLattitude;
 				} else {
 					initialSector += "1";
-					minLattitude += halfLongitude;
+					minLattitude += halfLattitude;
 				}
 			}
 		}
