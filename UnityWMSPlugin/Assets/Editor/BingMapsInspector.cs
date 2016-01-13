598130faa9e3be148f9a38ec599b9e948261d601
moisesjbc <moisesjbc@gmail.com>
2016-01-13 12:13:38 +0000

    Removed sliders from BingMaps inspector.

--- UnityWMSPlugin/Assets/Editor/BingMapsInspector.cs
+++ UnityWMSPlugin/Assets/Editor/BingMapsInspector.cs
@@ -8,6 +8,20 @@ using System.IO;
 [CustomEditor(typeof(BingMapsComponent))]
 public class BingMapsInspector : Editor
 {
+	const float MIN_LATTITUDE = -90.0f;
+	const float MAX_LATTITUDE = 90.0f;
+
+	const float MIN_LONGITUDE = -180.0f;
+	const float MAX_LONGITUDE = 180.0f;
+
+	const int MIN_ZOOM = 0;
+	const int MAX_ZOOM = 7;
+
+	static string lattitudeLabel = "Lattitude (" + MIN_LATTITUDE + ", " + MAX_LATTITUDE + "): ";
+	static string longitudeLabel = "Longitude (" + MIN_LONGITUDE + ", " + MAX_LONGITUDE + "): ";
+	static string zoomLabel = "Zoom (" + MIN_ZOOM + ", " + MAX_ZOOM + ")";
+
+
 	public override void OnInspectorGUI()
 	{
 		BingMapsComponent bingMapsComponent = (BingMapsComponent)target;
@@ -15,9 +29,9 @@ public class BingMapsInspector : Editor
 
 		EditorGUILayout.LabelField (bingMapsComponent.CurrentFixedUrl());
 
-		bingMapsComponent.lattitude = EditorGUILayout.Slider ("Lattitude: ", bingMapsComponent.lattitude, -90.0f, 90.0f);
-		bingMapsComponent.longitude = EditorGUILayout.Slider("Longitude: ", bingMapsComponent.longitude, -180.0f, 180f);
-		bingMapsComponent.initialZoom = EditorGUILayout.IntSlider ("Zoom: ", bingMapsComponent.initialZoom, 0, 7);
+		bingMapsComponent.lattitude = EditorGUILayout.FloatField(lattitudeLabel, bingMapsComponent.lattitude);
+		bingMapsComponent.longitude = EditorGUILayout.FloatField(longitudeLabel, bingMapsComponent.longitude);
+		bingMapsComponent.initialZoom = EditorGUILayout.IntField (zoomLabel, bingMapsComponent.initialZoom);
 		bingMapsComponent.ComputeInitialSector ();
 
 		if (GUI.changed) {
