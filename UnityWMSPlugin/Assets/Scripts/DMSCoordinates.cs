736d4f42a74827ee658502223a66ed87d132d172
moisesjbc <moisesjbc@gmail.com>
2016-01-14 11:45:24 +0000

    Added auxiliar DMSCoordinates classes.

---
+++ UnityWMSPlugin/Assets/Scripts/DMSCoordinates.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DMSCoordinates 
+{
+	public int degrees;
+	public int minutes;
+	public int seconds;
+
+	public float ToDecimalCoordinates()
+	{
+		return (float)degrees + (float)minutes / 60.0f + (float)seconds / 60.0f;
+	}
+};
+
+
+public class Lattitude : DMSCoordinates
+{
+}
+
+
+public class Longitude : DMSCoordinates
+{
+}
