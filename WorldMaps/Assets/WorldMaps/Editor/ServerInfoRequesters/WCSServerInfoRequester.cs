cac056692256ed0ac08898d39ac02e8263934e74
moisesjbc <moisesjbc@gmail.com>
2016-01-22 09:23:37 +0000

    Added WCSServerInfoRequester.

---
+++ WorldMaps/Assets/WorldMaps/Editor/ServerInfoRequesters/WCSServerInfoRequester.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WCSServerInfoRequester : ServerInfoRequester<WCSServerInfo> {
+	protected override WCSServerInfo ParseResponse( string responseText )
+	{
+		return WCSServerInfoXMLParser.GetWCSServerInfo (responseText);
+	}
+
+
+	protected override string BuildQueryString()
+	{
+		return "?REQUEST=GetCapabilities&SERVICE=WCS&VERSION=1.0.0";
+	}
+}
