03adb29b7802f79c8707802ebd12dceebbd1f93e
moisesjbc <moisesjbc@gmail.com>
2016-01-22 09:18:18 +0000

    Added empty WCSServerInfo.

---
+++ WorldMaps/Assets/WorldMaps/Editor/ServerInfo/WCSServerInfo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class WCSServerInfo
+{
+	[SerializeField]
+	public string label = null;
+
+	public WCSServerInfo( string label )
+	{
+		this.label = label;
+	}
+}
