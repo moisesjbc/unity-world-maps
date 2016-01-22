2af49f6f1db98a921498dbc82155367e7ac1d811
moisesjbc <moisesjbc@gmail.com>
2016-01-22 09:21:36 +0000

    Added empty WCSServerInfo XML parser.

---
+++ WorldMaps/Assets/WorldMaps/Editor/ServerInfoParsers/WCSServerInfoXMLParser.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Xml;
+
+public class WCSServerInfoXMLParser {
+	
+	public static WCSServerInfo GetWCSServerInfo( string xmlString ){
+		XmlDocument xmlDocument = new XmlDocument ();
+		xmlDocument.LoadXml (xmlString);
+
+		//XmlNode rootNode = xmlDocument.DocumentElement;
+
+		// Parse server general info.
+		//string serverTitle = rootNode.SelectSingleNode("Service").SelectSingleNode ("Title").InnerText;
+
+		return new WCSServerInfo( "<server label>" );
+	}
+}
